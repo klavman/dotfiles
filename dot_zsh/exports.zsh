ef1c06eaf59f03cfa6d5d3e618315e0c372e7907
Fco. Javier Clavero Álvarez <jclaveroalvarez@gmail.com>
2024-10-01 22:58:01 +0200

    Update exports

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -26,12 +26,11 @@ export ENV_DIR="$HOME/.local/share/venvs"
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-
-# export PATH=/snap/bin:$PATH
+export PATH=/snap/bin:$PATH
 # export PATH=$HOME/flutter/bin:$PATH
